a06ece672e22107a6decdd6bc32bcf87c71670b7
fnixdev <luisgatn000@gmail.com>
2022-08-02 09:14:47 -0300

    ++

--- resources/scripts/arch_install.sh
+++ resources/scripts/arch_install.sh
@@ -6,15 +6,9 @@ fi
 
 echo "★ - Intall script HilzuUB - ★"
 
-cmd="sudo pacman -Sy"
-packages="tree wget2 p7zip-full ffmpeg wget git neofetch python-pip"
-
 echo "[★] - Checking required packages:"
 
-for package in $packages ; do
-    cmd="$cmd $package"
-done
-
+sudo pacman -Sy tree wget p7zip ffmpeg wget git neofetch python-pip --noconfirm
 
 echo "[✔] - All packages are properly installed."
 
@@ -40,13 +34,20 @@ else
         python -m venv hilzu-venv
         echo "[✔] - hilzu-venv created"
         source $HOME/Loader/hilzu-venv/bin/activate
-        echo "[★] - Installing requeriments"
-        bash install_req
-        echo "[✔] - Requeriments installed."
     } || {
         echo "[⚠] - Could not create Virtual Env."
         exit 1
     }
+fi
+
+echo "[★] - Installing requeriments"
+sudo bash install_req
+echo "[✔] - Requeriments installed."
+
+echo -n "[ ? ] - It is likely that some modules are not installed even with script, do you want to run fix?"
+read answer </dev/tty
+if [[ "$answer" != "${answer#[Yy]}" ]]; then
+    sudo pip install heroku3
 fi
 
 cp config.env.sample config.env
@@ -56,5 +57,6 @@ read answer </dev/tty
 if [[ "$answer" != "${answer#[Yy]}" ]]; then
     bash genStr
 fi
+
 
 printf "\n[✔] - Installation complete, configure your config.env and start the bot with bash run\n"
