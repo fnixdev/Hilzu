3fc8c6f16a616ead3daaea12580d0f9355a1dbda
fnixdev <luisgatn000@gmail.com>
2022-08-02 08:29:52 -0300

    add script arch

---
+++ resources/scripts/arch_install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+if [ $(id -u) = 0 ]; then
+   echo "[⚠] - This script should not be run as root or sudo. Run with bash arch_install.sh, going out..."
+   exit 1
+fi
+
+echo "★ - Intall script HilzuUB - ★"
+
+cmd="sudo pacman -Sy"
+packages="tree wget2 p7zip-full ffmpeg wget git neofetch python-pip"
+
+echo "[★] - Checking required packages:"
+
+for package in $packages ; do
+    cmd="$cmd $package"
+    fi
+done
+
+
+echo "[✔] - All packages are properly installed."
+
+
+cd $HOME
+
+if [ -d "$HOME/Loader" ] ; then
+    echo "[★] - Folder Loader already exists"
+    echo "[★] - Removing old installation"
+    rm -rf /Loader
+fi
+
+echo "[★] - Cloning Loader in \"$HOME\""
+
+git clone https://github.com/fnixdev/Loader.git
+cd Loader
+
+if [ -d "$HOME/Loader/hilzu-venv" ] ; then
+    echo "[★] - hilzu-venv already exists"
+else
+    {
+        echo "[★] - Creating hilzu-venv and installing reqs."
+        python -m venv hilzu-venv
+        echo "[✔] - hilzu-venv created"
+        source $HOME/Kanna-X/hilzu-venv/bin/activate
+        echo "[★] - Installing requeriments"
+        bash install_req
+        echo "[✔] - Requeriments installed."
+    } || {
+        echo "[⚠] - Could not create Virtual Env."
+        exit 1
+    }
+fi
+
+cp config.env.sample config.env
+
+echo -n "[ ? ] - Do you want to generate your string session now? (Yy/Nn)?"
+read answer </dev/tty
+if [[ "$answer" != "${answer#[Yy]}" ]]; then
+    bash genStr
+fi
+
+printf "\n[✔] - Installation complete, configure your config.env and start the bot with bash run\n"
--- resources/scripts/ubuntu_install.sh
+++ resources/scripts/ubuntu_install.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
 
+echo "[★] - Intall script HilzuUB - ★"
 
-echo "Installing packages"
+echo
+
+echo "[★] - Installing packages"
 sudo apt-get update -y
 sudo apt-get install tree wget2 p7zip-full jq ffmpeg wget git neofetch mediainfo screen -y
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
 sudo rm google-chrome-stable_current_amd64.deb
 
-echo "Installing Python3.9..."
-sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt install python3.9 -y
-sudo apt install python3-pip
+## == 
 
-echo "Cloning repo"
+py3="python3"
+package=$(dpkg --get-selections | grep $py3 ) 
+echo 
+echo -n "[★] - Checking if package $py3 is installed."
+sleep 2
+
+if [ -n "$package" ] ;
+then echo
+    echo "[✔] - $py3 it's already installed, skipping"
+else echo
+    echo "[★] - Package $py3 not found, installing..."
+    sudo apt install software-properties-common -y
+    sudo add-apt-repository ppa:deadsnakes/ppa -y
+    sudo apt install python3.9 python-pip -y
+fi
+echo "[✔] - All packages are properly installed."
+
 cd ~
-git clone https://github.com/fnixdev/Loader.git && cd Loader
+echo "[★] - Cloning Loader"
 
-echo "Installing requirements"
+git clone https://github.com/fnixdev/Loader.git
+cd Loader
+
+echo "[★] - Installing requirements"
 bash install_req
 
-echo -n "Do you want to generate your string session now? (y/n)?"
+echo -n "[ ? ] - Do you want to generate your string session now? (Yy/Nn)?"
 read answer </dev/tty
 if [[ "$answer" != "${answer#[Yy]}" ]]; then
     bash genStr
